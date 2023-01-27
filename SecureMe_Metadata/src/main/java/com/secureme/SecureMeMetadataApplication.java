7c428228e0560624b87a7b68e26e3ae12097e3bb
ShubhamTheSavior <shubham.gadekar990@gmail.com>
2023-01-27 22:59:25 +0530

    pushed eureka and config server,client configurations

--- SecureMe_Metadata/src/main/java/com/secureme/SecureMeMetadataApplication.java
+++ SecureMe_Metadata/src/main/java/com/secureme/SecureMeMetadataApplication.java
@@ -2,8 +2,10 @@ package com.secureme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SecureMeMetadataApplication {
 
 	public static void main(String[] args) {
